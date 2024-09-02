383b06d279d50ad85c7738169c8ece0ca96765d4
정진석 <96866047+Jinseokk92@users.noreply.github.com>
2024-09-02 15:22:03 +0900

    Title: 변수 값 교체하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240902/변수 값 교체하기/replacing-variable-values.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        
+        // 변수 a에 값을 할당
+        int a = 3;
+        a = 6;
+        System.out.println(a);
+    }
+}
