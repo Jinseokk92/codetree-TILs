c9c5bbaf5da6d3ac2bb9901652b368083abc1e84
정진석 <96866047+Jinseokk92@users.noreply.github.com>
2024-09-02 16:55:45 +0900

    Title: 합을 복사하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240902/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        // 정수 a, b, c에 값을 할당
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        // 세 정수의 합 d
+        int d = a + b + c;
+
+        // a, b, c에 d의 값을 할당
+        a = b = c = d;
+
+        System.out.println(a + " " + b + " " + c);
+        
+    }
+}
