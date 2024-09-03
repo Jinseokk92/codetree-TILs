0278b923651642de476e6003bd09e93b1dab6f34
정진석 <96866047+Jinseokk92@users.noreply.github.com>
2024-09-03 13:05:58 +0900

    Title: 합과 평균의 차, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240903/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum  = a + b + c;
+        int avg = (a + b + c) / 3;
+
+        int result = sum - avg;
+
+        System.out.println(sum);
+        System.out.println(avg);
+        System.out.println(result);
+
+
+    }
+}
