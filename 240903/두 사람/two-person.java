12544928dbb9fae82a7b5ff55c33b80cef3acbec
정진석 <96866047+Jinseokk92@users.noreply.github.com>
2024-09-03 17:54:03 +0900

    Title: 두 사람, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240903/두 사람/two-person.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        
+        // A에 대한 나이와 성별
+        int a_age = sc.nextInt();
+        char a_sex = sc.next().charAt(0);
+
+        // B에 대한 나이와 성별
+        int b_age = sc.nextInt();
+        char b_sex = sc.next().charAt(0);
+
+        if ( (a_age >= 19 && a_sex == 'M') || (b_age >= 19 && b_sex == 'M' ) ) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
