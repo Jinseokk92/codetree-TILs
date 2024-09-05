6a3ad8779fe37d7e26a12469fe07e401fa638a00
정진석 <96866047+Jinseokk92@users.noreply.github.com>
2024-09-05 17:46:00 +0900

    Title: 3n + 1 수열, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240905/3n + 1 수열/3n-plus-1-sequence.java
+++ 240905/3n + 1 수열/3n-plus-1-sequence.java
@@ -9,16 +9,17 @@ public class Main {
         int N = sc.nextInt();
 
         while ( true ) {
+
+            if ( N == 1 ) {
+                break;
+            }
+            
             if ( N % 2 == 0 ) {
                 N /= 2;
                 cnt++;
             } else {
                 N = (N * 3) + 1;
                 cnt++;
-            }
-
-            if ( N == 1 ) {
-                break;
             }
         }
 
