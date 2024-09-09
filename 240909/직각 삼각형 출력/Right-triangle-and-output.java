eaa68245006e424e7b0e19987b041b160981562d
정진석 <96866047+Jinseokk92@users.noreply.github.com>
2024-09-09 17:08:32 +0900

    Title: 직각 삼각형 출력, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240909/직각 삼각형 출력/Right-triangle-and-output.java
+++ 240909/직각 삼각형 출력/Right-triangle-and-output.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         for ( int i = 0; i < n; i++ ) {
-            for ( int j = 0; j < i ; j++ ) {
+            for ( int j = 0; j < (2 * i) + 1 ; j++ ) {
                 System.out.print("*");
             } 
             System.out.println();
